3541116d6500bc2caadbcfb4e458b75c17ac2966
Windows <ksm.emails@gmail.com>
2023-11-26 08:50:31 +0100

    save

--- _sklearn/func/+mutual_info_regression.py
+++ _sklearn/func/+mutual_info_regression.py
@@ -7,14 +7,14 @@ import seaborn as sns
 # Generate some random data
 np.random.seed(42)
 X = pd.DataFrame({
-    'A': [1, 2, 2, 4, 1],
-    'B': [4, 3, 2, 1, 1],
-    'R': [1, 0, 0, 1, 1]
+    'A': [1, 2, 2, 4, 1, 4, 5, 6, 7],
+    'B': [4, 3, 2, 1, 1, 2, 2, 1, 0],
+    'R': [1, 0, 0, 1, 1, 0, 0, 1, 1]
 })
-y = pd.Series([1, 0, 0, 1, 1])  # Convert y to a pandas Series
+y = X["R"]  # Convert y to a pandas Series
 
 def make_mi_scores(X, y, discrete_features):
-    print(X,y)
+    print(X, y)
     mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
     mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
     mi_scores = mi_scores.sort_values(ascending=False)
@@ -36,3 +36,15 @@ print(mi_scores)  # Show a few features with their MI scores
 plt.figure(dpi=100, figsize=(8, 5))
 plot_mi_scores(mi_scores)
 plt.show()
+
+
+features = ["A", "B"]
+sns.relplot(
+    x="value", y="R", col="variable", data=X.melt(id_vars="R", value_vars=features), facet_kws=dict(sharex=False),
+);
+plt.show()
+
+
+sns.catplot(x="A", y="R", data=X, kind="boxen")
+plt.show()
+
