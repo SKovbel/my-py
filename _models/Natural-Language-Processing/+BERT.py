13bdd8d3db91241d96292107d03d6ea561ca049e
Windows <ksm.emails@gmail.com>
2023-11-30 00:32:10 +0100

    save

--- _models/Natural-Language-Processing/+BERT.py
+++ _models/Natural-Language-Processing/+BERT.py
@@ -1,23 +1,85 @@
+# BERT is a pre-trained natural language processing (NLP) model developed by Google.
+# It belongs to the transformer architecture, which is a type of neural network architecture that has been highly successful in various NLP tasks.
+# BERT is pre-trained on a massive amount of text data and can be fine-tuned for specific NLP tasks such as question answering, sentiment analysis, and more.
+# It has been influential in advancing the state-of-the-art in NLP.
+# Install the required libraries
+
 import torch
-from transformers import BertTokenizer, BertModel
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
+
+# Assuming you have a dataset with text and corresponding labels
+# Example data: text_data, labels
+text_data = ["This is a sample sentence.", "Another example sentence."]
+labels = [0, 1]  # Replace with your actual labels
+
+# Split the data into training and testing sets
+train_texts, test_texts, train_labels, test_labels = train_test_split(text_data, labels, test_size=0.2, random_state=42)
 
 # Load pre-trained BERT model and tokenizer
-bert_model_name = 'bert-base-uncased'
-bert_tokenizer = BertTokenizer.from_pretrained(bert_model_name)
-bert_model = BertModel.from_pretrained(bert_model_name)
+model_name = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name, num_labels=len(set(labels)))
 
-# Example input text
-text = "Once upon a time in a land far away."
+# Tokenize the input data
+train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=128, return_tensors='pt')
+test_encodings = tokenizer(test_texts, truncation=True, padding=True, max_length=128, return_tensors='pt')
 
-# Tokenize input text
-input_ids = bert_tokenizer.encode(text, return_tensors='pt')
+# Create PyTorch datasets
+class CustomDataset(Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = torch.tensor(labels)
 
-# Get embeddings from BERT
-outputs = bert_model(input_ids)
-embeddings = outputs.last_hidden_state
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
 
-text_from_embeddings = bert_tokenizer.decode(outputs, skip_special_tokens=True)
+    def __len__(self):
+        return len(self.labels)
 
-# Print the original text and the text from embeddings
-print("Original Text:", text)
-print("Text from Embeddings:", text_from_embeddings)
+train_dataset = CustomDataset(train_encodings, train_labels)
+test_dataset = CustomDataset(test_encodings, test_labels)
+
+# Create DataLoader for training and testing
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
+
+# Fine-tune the BERT model
+optimizer = AdamW(model.parameters(), lr=1e-5)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model.to(device)
+
+num_epochs = 3
+for epoch in range(num_epochs):
+    model.train()
+    for batch in tqdm(train_loader):
+        optimizer.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+
+# Evaluate the fine-tuned model on the test set
+model.eval()
+predictions = []
+with torch.no_grad():
+    for batch in tqdm(test_loader):
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        predictions.extend(logits.argmax(dim=1).cpu().numpy())
+
+# Calculate accuracy
+accuracy = accuracy_score(test_labels, predictions)
+print(f'Accuracy: {accuracy}')
+
