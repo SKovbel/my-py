525109ef2d8971d3d7228ccb7083b48d3a16da10
KSM <ksm.emails@gmail.com>
2023-12-20 22:47:58 +0100

    save

---
+++ _python/os.py
@@ -0,0 +1,21 @@
+import os
+
+
+# 1
+PATH = os.path.join(os.getcwd(), f"../../")
+
+# 2
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+# 3
+DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keys.json')
+
+# 4
+path = lambda name: os.path.join(s.getcwd(), name)
+path = lambda name: os.path.join(os.path.dirname(os.path.abspath(__file__)), name)
+
+$ 5
+for dir in os.listdir(DIR):
+    dir = os.path.join(DIR, dir)
+    if os.path.isdir(dir):
+        pass
