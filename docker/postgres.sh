31553be4f9ca3ffbebca2dd98764198c787dd7c1
KSM <ksm.emails@gmail.com>
2024-01-30 23:37:46 +0100

    save

--- docker/postgres.sh
+++ docker/postgres.sh
@@ -1,9 +1,8 @@
-mkdir -p data/pgsql
+mkdir -p ./../tmp/pgsql
 
-python3.13 -m venv ../tmp/db-venv
-source ../tmp/db-venv/bin/activate
-pip install docker-compose
-
+#python3.11 -m venv ../tmp/db-venv
+#source ../tmp/db-venv/bin/activate
+#pip install docker-compose
 
 sudo chmod 666 /var/run/docker.sock
 docker-compose -f cnf/postgres.yml up
--- docker/teradata.sh
+++ docker/teradata.sh
@@ -1,9 +1,11 @@
-mkdir -p data/pgsql
+mkdir -p data/terada
 
-python3.13 -m venv ../tmp/db-venv
-source ../tmp/db-venv/bin/activate
-pip install docker-compose
+#python3.13 -m venv ../tmp/db-venv
+#source ../tmp/db-venv/bin/activate
+#pip install docker-compose
 
 
 sudo chmod 666 /var/run/docker.sock
+
+docker login
 docker-compose -f cnf/teradata.yml up
